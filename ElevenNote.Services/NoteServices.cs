dcb8facd986e90209c1491a96444fd0e31af14f2
mdskinner31 <mdskinner31@gmail.com>
2021-04-07 12:02:32 -0400

    Note Service

---
+++ ElevenNote.Services/NoteServices.cs
@@ -0,0 +1,63 @@
+﻿using ElevenNote.Data;
+using ElevneNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteServices
+    {
+        private readonly Guid _userId;
+
+        public NoteServices(Guid userId)
+        {
+            _userId = userId;
+        }
+
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new NoteListItem
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
+
+
